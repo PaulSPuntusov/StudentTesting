6acdceb79d00dae3e9cea9d8cea08bdbbce6c6a9
PaulSPuntusov <pps_ru@mail.ru>
2024-10-31 22:05:05 +0300

    победу не победил

--- src/test/java/ru/PicabuTest.java
+++ src/test/java/ru/PicabuTest.java
@@ -12,14 +12,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class PicabuTest {
     WebDriver driver;
+    WebDriverWait wait;
+    WebElement element;
 
     @BeforeMethod
             (alwaysRun = true)
@@ -41,14 +47,15 @@ public class PicabuTest {
 
     @Test
     public void pobeda() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10L));
         driver.findElement(By.xpath("//textarea[contains(@class,\"gLFyf\")]")).sendKeys("Сайт авиакомпании Победа");
-        driver.findElement(By.xpath("//textarea[contains(@class,\"gLFyf\")]")).sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//*[@id=\"rso\"]/div[2]/div[1]/div/div/div/div[1]/div/div/span/a/div/div/div/div[2]/cite")).click();
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/main/div/div/div[1]/div/div[1]/button[4]/div[2]/div/div"));
+        driver.findElement(By.xpath("//textarea[contains(@class,\"gLFyf\")]")).sendKeys(Keys.ENTER); // кликаю именно pobeda.aero
+        driver.findElement(By.xpath("//*[@id=\"rso\"]/div[2]/div[1]/div/div/div/div[1]/div/div/span/a/div/div/div/div[2]/cite")).click(); // ищу именно pobeda.aero
+        element = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/main/div/div/div[1]/div/div[1]/button[4]/div[2]/div/div"));// ищу Калининград
         waitForVisibilityOfElement(element);
-        wait.until(elementToBeClickable(driver.findElement
-                (By.xpath("//*[@id=\"__next\"]/div[2]/header/div/div/div[1]/div[2]/button[1]"))));
-        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/header/div/div/div[1]/div[2]/button[1]")).click();
+        wait.until(visibilityOf(driver.findElement(By.xpath("//*[@id=\"__next\"]/div[2]/header/div/div/div[1]/div[2]/button[1]")))); // кнопка переклчения языка - жду кликабельности
+
+        driver.findElement(By.cssSelector("#__next > div.dp-lw1vya-root > header > div > div > div.dp-4ksyid-root-root > div.dp-qq7t6o-root > button.dp-bi33jb-root-root")).click(); // кнопка переклчения языка нажимаю
     }
 
     public static void waitForVisibilityOfElement(WebElement element) {
